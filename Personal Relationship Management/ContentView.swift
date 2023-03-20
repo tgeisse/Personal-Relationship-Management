2d7c637761746fe3ca7a8111caf32bdc72f89cfc
Taylor Geisse <1383010+tgeisse@users.noreply.github.com>
2023-03-20 09:37:52 -0700

    Initial Commit

---
+++ Personal Relationship Management/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Personal Relationship Management
+//
+//  Created by Taylor Geisse on 3/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Personal Relationship Management/Personal_Relationship_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Relationship_ManagementApp.swift
+//  Personal Relationship Management
+//
+//  Created by Taylor Geisse on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Relationship_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
